3806490e91ffb46fa3e226d9b01dde4f3cf177a3
SH-OH <dhtkdgh429@gmail.com>
2020-10-22 14:25:43 +0900

    Add Deployment Target 12.0
    Add AppLaunch 추가

---
+++ Unsplash-SHOH/App/AppLaunch.swift
@@ -0,0 +1,18 @@
+//
+//  AppLaunch.swift
+//  Unsplash-SHOH
+//
+//  Created by Oh Sangho on 2020/10/22.
+//
+
+import UIKit.UIWindow
+
+struct AppLaunch {
+    static func launchByOS(_ window: UIWindow?, isScene: Bool) {
+        if #available(iOS 13.0, *) {
+            guard isScene else { return }
+        }
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
